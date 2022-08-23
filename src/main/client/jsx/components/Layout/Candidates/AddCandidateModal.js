78676cdad9c7a1d5c7288503fb0d9219151cf73b
henriquegon <henrique.goncalves@supersim.com.br>
2022-08-23 20:11:06 -0300

    - Adding components

---
+++ src/main/client/jsx/components/Layout/Candidates/AddCandidateModal.js
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+import Modal from "../../UI/Modal";
+import Input from "../../UI/Input";
+import Button from "../../UI/Button";
+import Select from "../../UI/Select";
+
+import classes from "./AddCandidateModal.module.css"
+
+const phases = [
+    { id: 1, value: "application", name: "Application" },
+    { id: 2, value: "approved", name: "Approved" },
+    { id: 3, value: "declined", name: "Declined" }
+]
+
+const areas = [
+    { id: 1, value: "data", name: "Data" },
+    { id: 2, value: "tech", name: "Tech" },
+    { id: 3, value: "risk", name: "Risk" },
+    { id: 4, value: "legal", name: "Legal" }
+]
+
+const AddCandidateModal = (props) => {
+    const nameRef = useRef()
+    const phaseRef = useRef()
+    const positionRef = useRef()
+    const areaRef = useRef()
+
+    const cleanRef = refs => {
+        for(let ref of refs){
+            ref.current.value = ''
+        }
+    }
+
+    async function addCandidateHandler (candidateData) {
+        const response = await fetch('http://localhost:8080/api/candidate', {
+            method: 'POST', 
+            body: JSON.stringify(candidateData), 
+            headers: {'Content-Type': 'application/json'
+        }})
+        const data = await response.json()
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const data = {
+            name: nameRef.current.value,
+            phase: phaseRef.current.value,
+            position: positionRef.current.value,
+            area: areaRef.current.value,
+        }
+        addCandidateHandler(data)
+        cleanRef([nameRef, phaseRef, positionRef, areaRef])
+        props.onClose()
+    }
+
+    return (
+        <Modal onClose={props.onClose}>
+            <form className={classes["add-candidate-modal"]} onSubmit={submitHandler}>
+                <Input ref={nameRef} label="Name"/>
+                <Input ref={positionRef} label="Position"/>
+                <Select 
+                    label="Phase" 
+                    ref={phaseRef} 
+                    options={phases}
+                />
+                <Select 
+                    label="Area" 
+                    ref={areaRef} 
+                    options={areas} 
+                />
+                 <Button label="Create"/>
+            </form>
+        </Modal>
+    )
+}
+
+export default AddCandidateModal 
