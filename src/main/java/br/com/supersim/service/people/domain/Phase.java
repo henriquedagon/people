7e341770035b9319c49af978a9bd69f2f4d0232e
henriquegon <henrique.goncalves@supersim.com.br>
2022-08-23 20:20:32 -0300

    - Adding phase enum

--- src/main/java/br/com/supersim/service/people/domain/Phase.java
+++ src/main/java/br/com/supersim/service/people/domain/Phase.java
@@ -1,4 +1,53 @@
 package br.com.supersim.service.people.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Phase {
+
+    APPLICATION(0L, "application", "Application"),
+
+    DECLINED(10L, "declined", "Declined"),
+
+    APPROVED(20L, "approved", "Approved")
+
+    ;
+
+    private Long id;
+
+    private String value;
+
+    private String name;
+
+    Phase(Long id, String value, String name) {
+        this.id = id;
+        this.value = value;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Map<String, Object> toJson(Phase phase) {
+        return Map.of("id", phase.getId(),
+                "value", phase.getValue(),
+                "name",phase.getName());
+    }
+
+    public static List<Map<String, Object>> getAllAsJson() {
+        return Arrays.stream(Phase.values())
+                .map(Phase::toJson)
+                .collect(Collectors.toList());
+    }
 }
