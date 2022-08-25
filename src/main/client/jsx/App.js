ed13630dbd01285561ea394f4aaa1de17c6ff317
henriquegon <henrique.goncalves@supersim.com.br>
2022-08-25 16:45:35 -0300

    - Adding welcoming page

--- src/main/client/jsx/App.js
+++ src/main/client/jsx/App.js
@@ -1,12 +1,24 @@
 import React from "react"
 import Header from "./components/Layout/Candidates/Header";
 import Candidates from "./components/Layout/Candidates/Candidates";
+import { Redirect, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <Candidates/>
+      <main>
+        <Route path="/" exact>
+          <Redirect to="/welcome"/>
+        </Route>
+        <Route path="/welcome">
+          <Welcome/>
+        </Route>
+        <Route path="/candidates">
+          <Candidates/>
+        </Route>
+      </main>
     </React.Fragment>
 );
 }
--- src/main/client/jsx/components/Layout/Candidates/Header.js
+++ src/main/client/jsx/components/Layout/Candidates/Header.js
@@ -1,14 +1,43 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import classes from "./Header.module.css"
 
-const Header = props => {
+const Header = () => {
     return (
         <header className={classes['main-header']}>
             <div>
-                <h1>People</h1>
+                <Link to="/welcome" className={classes['main-header__brand']}>
+                    <h1>People</h1>
+                </Link>
             </div>
+            <ul>
+                <li>
+                    <NavLink to="/candidates">Candidates</NavLink>
+                </li>
+            </ul>
         </header>
     )
 }
 
 export default Header 
+
+    // <header class="main-header">
+    //     <div>
+    //         <a class="main-header__brand" href="index.html">
+    //             uHost
+    //         </a>
+    //     </div>
+    //     <nav class="main-nav">
+    //         <ul class="main-nav__items">
+    //             <li class="main-nav__item">
+    //                 <a href="packages/index.html">Packages</a>
+    //             </li>
+    //             <li class="main-nav__item">
+    //                 <a href="customers/index.html">Customers</a>
+    //             </li>
+    //             <li class="main-nav__item main-nav__item--cta">
+    //                 <a href="start-hosting/index.html">Start Hosting</a>
+    //             </li>
+    //         </ul>
+    //     </nav>
+    // </header>
