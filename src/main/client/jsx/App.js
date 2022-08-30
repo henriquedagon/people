3c6ef723c743b3061326cf162e0d888a5ed3ee26
henriquegon <henrique.goncalves@supersim.com.br>
2022-08-30 16:05:28 -0300

    - Adding Switch

--- src/main/client/jsx/App.js
+++ src/main/client/jsx/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./components/Layout/Candidates/Header";
 import Candidates from "./pages/Candidates";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Candidate from "./pages/Candidate";
 
@@ -10,18 +10,20 @@ function App() {
     <React.Fragment>
       <Header/>
       <main>
-        <Route path="/" exact>
-          <Redirect to="/welcome"/>
-        </Route>
-        <Route path="/welcome">
-          <Welcome/>
-        </Route>
-        <Route path="/candidate" exact>
-          <Candidates/>
-        </Route>
-        <Route path="/candidate/:candidateId">
-          <Candidate/>
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"/>
+          </Route>
+          <Route path="/welcome">
+            <Welcome/>
+          </Route>
+          <Route path="/candidate" exact>
+            <Candidates/>
+          </Route>
+          <Route path="/candidate/:candidateId">
+            <Candidate/>
+          </Route>
+        </Switch>
       </main>
     </React.Fragment>
 );
