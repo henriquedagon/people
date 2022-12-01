0ffa829d4ac61567eae99334696cbcfca0651d19
henriquegon <henrique.goncalves@supersim.com.br>
2022-12-01 12:31:28 -0300

    - Add CODEOWNERS

--- src/main/java/br/com/supersim/service/people/common/HashMapConverter.java
+++ src/main/java/br/com/supersim/service/people/common/HashMapConverter.java
@@ -1,6 +1,22 @@
 package br.com.supersim.service.people.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.AttributeConverter;
+import java.io.IOException;
+import java.util.Map;
+
 public class HashMapConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private static final Logger LOGGER =  LoggerFactory.getLogger(HashMapConverter.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public String convertToDatabaseColumn(Map<String, Object> customerInfo) {
 
@@ -8,7 +24,7 @@ public class HashMapConverter implements AttributeConverter<Map<String, Object>,
         try {
             customerInfoJson = objectMapper.writeValueAsString(customerInfo);
         } catch (final JsonProcessingException e) {
-            logger.error("JSON writing error", e);
+            HashMapConverter.LOGGER.error("JSON writing error", e);
         }
 
         return customerInfoJson;
@@ -21,7 +37,7 @@ public class HashMapConverter implements AttributeConverter<Map<String, Object>,
         try {
             customerInfo = objectMapper.readValue(customerInfoJSON, Map.class);
         } catch (final IOException e) {
-            logger.error("JSON reading error", e);
+            HashMapConverter.LOGGER.error("JSON reading error", e);
         }
 
         return customerInfo;
